c1cb060abaa2c5e44895ef231d1a93dc0f402ba1
Kyle <kylepcole@gmail.com>
2019-12-04 16:48:52 -0500

    author search

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,12 @@ class AuthorsController < ApplicationController
 	before_action :find_author, only: [:show]
 
 	def index
-		@authors = Author.all
+		if Author.search(params[:search]) == []
+			flash[:error] = "No Results Found"
+			@authors = Author.all
+		else
+			@authors = Author.search(params[:search])
+		end
 	end
 
 	private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,16 @@
 class Author < ApplicationRecord
 	has_many :books
+
+	def self.search(search)
+		if search
+			author = Author.where("name like ?", "%#{search}%")
+			if author
+				author
+			else
+				Author.all
+			end
+		else
+			Author.all
+		end
+	end
 end
