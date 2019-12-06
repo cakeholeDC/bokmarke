9c662824c69987504fbce5cea698fa8e10bb6a9f
Kyle <kylepcole@gmail.com>
2019-12-06 12:45:06 -0500

    show, index, and about pages

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,5 +7,10 @@ class Review < ApplicationRecord
   validates :rating, numericality: {only_integer: true, greater_than: 0, less_than: 6}
   validates :recommend, inclusion: { in: [true, false] }
 
-
+  def is_recommended?
+  	if self.recommend == true
+  		self.goal.reader.username + " recommends this book"
+  	end
+  end
+  
 end
